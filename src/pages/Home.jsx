35e4d5f6d6b9f1c7e7526fc63879854e3e23aa3e
Pdeepak18 <85224426+Pdeepak18@users.noreply.github.com>
2023-07-20 17:27:13 +0530

    Jai shree ram

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sliderimage from '../components/Sliderimage'
+
+const Home = () => {
+  return (
+    <div>
+      <Sliderimage />
+    </div>
+  )
+}
+
+export default Home
